ab4c1c8d9a1ad2cf20fd0d90caf0e424a9cddbf2
kentaro-wakabayashi <b162228m@st.u-gakugei.ac.jp>
2021-12-27 14:30:37 +0900

    空白の訂正

--- contactform/insert.php
+++ contactform/insert.php
@@ -1,11 +1,10 @@
 <?php
-mb_internal_encoding("utf8");
+    mb_internal_encoding("utf8");
 
-$pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
+    $pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","");
 
-$pdo->exec("insert into contactform(name,mail,age,comments)
-values('".$_POST['name']."','".$_POST['mail']."','".$_POST['age']."','".$_POST['comments']."');");
-
+    $pdo->exec("insert into contactform(name,mail,age,comments)
+    values('".$_POST['name']."','".$_POST['mail']."','".$_POST['age']."','".$_POST['comments']."');");
 ?>
 
 <!doctype HTML>
